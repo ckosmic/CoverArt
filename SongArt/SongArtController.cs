0e5eaa0b75fbde101f5d3f488cedd5b5a975c4ca
Christian Kosman <christiankosman@gmail.com>
2020-12-24 22:49:08 -0800

    Cleanup template comments, disable multiplicative blending option

--- SongArt/SongArtController.cs
+++ SongArt/SongArtController.cs
@@ -9,10 +9,6 @@ using UnityEngine;
 
 namespace SongArt
 {
-	/// <summary>
-	/// Monobehaviours (scripts) are added to GameObjects.
-	/// For a full list of Messages a Monobehaviour can receive from the game, see https://docs.unity3d.com/ScriptReference/MonoBehaviour.html.
-	/// </summary>
 	public class SongArtController : MonoBehaviour {
 		public static SongArtController Instance { get; private set; }
 
@@ -25,14 +21,7 @@ namespace SongArt
 		private Color envColor1;
 		private EnvironmentLight[] lights;
 
-		// These methods are automatically called by Unity, you should remove any you aren't using.
-		#region Monobehaviour Messages
-		/// <summary>
-		/// Only ever called once, mainly used to initialize variables.
-		/// </summary>
 		private void Awake() {
-			// For this particular MonoBehaviour, we only want one instance to exist at any time, so store a reference to it in a static property
-			//   and destroy any that are created while one already exists.
 			if (Instance != null) {
 				Plugin.Log?.Warn($"Instance of {GetType().Name} already exists, destroying.");
 				GameObject.DestroyImmediate(this);
@@ -42,17 +31,12 @@ namespace SongArt
 			Instance = this;
 			Plugin.Log?.Debug($"{name}: Awake()");
 		}
-		/// <summary>
-		/// Only ever called once on the first frame the script is Enabled. Start is called after any other script's Awake() and before Update().
-		/// </summary>
+
 		private void Start() {
 			AssetBundle bundle = AssetBundleManager.LoadAssetBundleFromResource($"SongArt.songart");
 			coverShader = bundle.LoadAsset<Shader>("Assets/Shaders/CoverArt.shader");
 		}
 
-		/// <summary>
-		/// Called every frame if the script is enabled.
-		/// </summary>
 		private void Update() {
 			if (coverQuad != null) {
 				// Perform fading
@@ -98,37 +82,12 @@ namespace SongArt
 			}
 		}
 
-		/// <summary>
-		/// Called every frame after every other enabled script's Update().
-		/// </summary>
-		private void LateUpdate() {
-
-		}
-
-		/// <summary>
-		/// Called when the script becomes enabled and active
-		/// </summary>
-		private void OnEnable() {
-			
-		}
-
-		/// <summary>
-		/// Called when the script becomes disabled or when it is being destroyed.
-		/// </summary>
-		private void OnDisable() {
-
-		}
-
-		/// <summary>
-		/// Called when the script is being destroyed.
-		/// </summary>
 		private void OnDestroy() {
 			Plugin.Log?.Debug($"{name}: OnDestroy()");
 			if (Instance == this)
 				Instance = null; // This MonoBehaviour is being destroyed, so set the static instance property to null.
 
 		}
-		#endregion
 
 		public void OnGameSceneLoaded() {
 			fadeTimer = Time.time + PluginConfig.Instance.fadeDelay;
